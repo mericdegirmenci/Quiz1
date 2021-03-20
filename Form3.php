28e455e93792264e94897ba5ed176b0dee9de62d
mericdegirmenci <1800003317@stu.iku.edu.tr>
2021-03-20 11:57:42 +0300

    My third commit that uses the Post method

---
+++ Form3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form 3</title>
+</head>
+<body>
+
+<form action="Form3.php" method="post">
+
+<input type="text" name="text">
+<input type="submit">
+
+</form>
+
+<pre>
+$_POST
+<?php
+print_r($_POST)
+?>
+</pre>
+    
+</body>
+</html>
